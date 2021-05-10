5af09b5d731aa7b5c5a0bc9df12ff22bb0052071
basilioarth <basilio.arth@gmail.com>
2021-05-10 10:05:28 -0300

    Refatorando a arquitetura do código

--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
 from unicodedata import normalize
 
+'''
+Devolve cópia de uma str substituindo os caracteres
+acentuados pelos seus equivalentes não acentuados.
+
+ATENÇÃO: carateres gráficos não ASCII e não alfa-numéricos,
+tais como bullets, travessões, aspas assimétricas, etc.
+são simplesmente removidos!
+
+>>> remover_acentos('[ACENTUAÇÃO] ç: áàãâä! éèêë? íìîï, óòõôö; úùûü.')
+'[ACENTUACAO] c: aaaaa! eeee? iiii, ooooo; uuuu.'
+'''
 def remove_accents(string):
     return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
 
-file = open('assets/municipios.txt', 'r', encoding='utf8')
+def transformMunicipios(municipio):
+    return remove_accents(municipio.lower()).replace(" ", "").replace("\n", "")
 
-for line in file:
-    print(remove_accents(line.lower()).replace(" ", ""))
+def getMunicipios(municipiosList):
+    file = open('assets/municipios.txt', 'r', encoding='utf8')
 
-file.close()
+    for municipio in file:
+        municipiosList.append(transformMunicipios(municipio))
+
+    file.close()
+
+if __name__ == '__main__':
+    municipios = []
+    getMunicipios(municipios)
+
+    for municipio in municipios:
+        print(municipio)
