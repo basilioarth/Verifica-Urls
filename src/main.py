d329152c19b3c4d0c1391a757d34b3d2e625a2c2
basilioarth <basilio.arth@gmail.com>
2021-05-10 19:53:43 -0300

    Tratando problemas de requisição e atrualizando o tratamento dos status

--- src/main.py
+++ src/main.py
@@ -3,9 +3,20 @@ from requests.exceptions import HTTPError
 from unicodedata import normalize
 import pandas as pd
 import time
-#from requests.adapters import HTTPAdapter
-#from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
 
+'''
+Devolve cópia de uma str substituindo os caracteres
+acentuados pelos seus equivalentes não acentuados.
+
+ATENÇÃO: carateres gráficos não ASCII e não alfa-numéricos,
+tais como bullets, travessões, aspas assimétricas, etc.
+são simplesmente removidos!
+
+>>> remover_acentos('[ACENTUAÇÃO] ç: áàãâä! éèêë? íìîï, óòõôö; úùûü.')
+'[ACENTUACAO] c: aaaaa! eeee? iiii, ooooo; uuuu.'
+''' 
 def remove_accents(string):
     return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
 
@@ -29,22 +40,30 @@ def createUrl(municipio, type_url):
         return 'http://' + municipio_url_prefix + '.pi.leg.br'
 
 def request(url):
-    status = "Fail"
+    status = "Success"
     request_response_message = ""
 
-    try: 
-        response = requests.get(url)
-        response.raise_for_status()
-    except HTTPError as http_err:
-        request_response_message = f'HTTP error occurred: {http_err}'
-        print(request_response_message)   
-    except Exception as err:
-        request_response_message = f'Other error occurred: {err}'
-        print(request_response_message)  
-    else:
-        request_response_message = response
-        status = "Success"
-    
+    adapter = HTTPAdapter(max_retries=5)
+    session = requests.Session()
+    session.mount(url, adapter)
+
+    headers = {
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+    } # To avoid "TooManyRedirects: Exceeded 30 redirects"
+
+    try:
+        response = session.get(url, headers = headers, verify = False) # "verify = false" to avoid SSL certificate errors 
+        request_response_message = response.status_code
+        if(int(request_response_message) >= 400): status = "Fail"
+    except ConnectionError as ce:
+        status = "Fail"
+        request_response_message = ce
     return [url, status, request_response_message]
     
 def testConnection(municipio):
@@ -123,16 +142,4 @@ if __name__ == '__main__':
         'RequestResponseLeg': RequestResponseLeg
     })
 
-    planilha.to_excel('assets/planilha_teste.xlsx')
-    
-'''
-Devolve cópia de uma str substituindo os caracteres
-acentuados pelos seus equivalentes não acentuados.
-
-ATENÇÃO: carateres gráficos não ASCII e não alfa-numéricos,
-tais como bullets, travessões, aspas assimétricas, etc.
-são simplesmente removidos!
-
->>> remover_acentos('[ACENTUAÇÃO] ç: áàãâä! éèêë? íìîï, óòõôö; úùûü.')
-'[ACENTUACAO] c: aaaaa! eeee? iiii, ooooo; uuuu.'
-''' 
+    planilha.to_excel('assets/MunicipiosUrls.xlsx')
