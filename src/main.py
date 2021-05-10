4cb453a9a21d9070c81e7b1a3143d1ab1a3f32be
basilioarth <basilio.arth@gmail.com>
2021-05-10 11:21:35 -0300

    Refatorando o código

--- src/main.py
+++ src/main.py
@@ -1,5 +1,60 @@
+import requests
 from unicodedata import normalize
 
+def remove_accents(string):
+    return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
+
+def adjustMunicipioName(municipio):
+    return remove_accents(municipio.lower()).replace(" ", "")
+
+def getMunicipios(municipiosList):
+    file = open('assets/municipios.txt', 'r', encoding='utf8')
+
+    for municipio in file:
+        municipiosList.append(municipio.replace("\n", ""))
+
+    file.close()
+
+def createUrl(municipio, type_url):
+    municipio_url_prefix = adjustMunicipioName(municipio)
+    
+    if type_url == 0:
+        return municipio_url_prefix + '.pi.gov.br'
+    else: 
+        return municipio_url_prefix + '.pi.leg.br'
+
+def testConnection(municipio):
+    gov = 0
+    leg = 1
+
+    urlGov = createUrl(municipio, gov)
+    urlLeg = createUrl(municipio, leg)
+
+    return [urlGov, urlLeg]
+    
+
+if __name__ == '__main__':
+    municipios = []
+    urls = []
+    count = 0
+
+    getMunicipios(municipios)
+    print(municipios)
+    
+    for municipio in municipios:
+        urls.append(testConnection(municipio))
+
+        '''
+        urlsGov.append(createUrlGov(municipio))
+        urlsLeg.append(createUrlLeg(municipio))
+
+        print(urlsGov[count])
+        print(urlsLeg[count])
+        print('\n')
+        count = count + 1
+    '''
+    print("Total de municípios: {}".format(len(municipios)))
+    print(urls)
 '''
 Devolve cópia de uma str substituindo os caracteres
 acentuados pelos seus equivalentes não acentuados.
@@ -10,45 +65,4 @@ são simplesmente removidos!
 
 >>> remover_acentos('[ACENTUAÇÃO] ç: áàãâä! éèêë? íìîï, óòõôö; úùûü.')
 '[ACENTUACAO] c: aaaaa! eeee? iiii, ooooo; uuuu.'
-'''
-def remove_accents(string):
-    return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
-
-def transformMunicipios(municipio):
-    return remove_accents(municipio.lower()).replace(" ", "").replace("\n", "")
-
-def getMunicipios(municipiosList):
-    file = open('assets/municipios.txt', 'r', encoding='utf8')
-
-    for municipio in file:
-        municipiosList.append(transformMunicipios(municipio))
-
-    file.close()
-
-def createUrlGov(municipio):
-    return municipio + '.pi.gov.br'
-
-def createUrlLeg(municipio):
-    return municipio + '.pi.leg.br'
-
-if __name__ == '__main__':
-    municipios = []
-    urlsGov = []
-    urlsLeg = []
-    count = 0
-
-    getMunicipios(municipios)
-
-    for municipio in municipios:
-        print(municipio)
-
-        urlsGov.append(createUrlGov(municipio))
-        urlsLeg.append(createUrlLeg(municipio))
-
-        print(urlsGov[count])
-        print(urlsLeg[count])
-        print('\n')
-        count = count + 1
-    
-    print("Total de municípios: {}".format(len(municipios)))
-    
+''' 
