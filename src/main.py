abb1a08e98e7cfce77a588c14f483c19c14f8df1
basilioarth <basilio.arth@gmail.com>
2021-05-10 09:07:41 -0300

    Removendo acentos

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-# Abrindo o arquivo
+from unicodedata import normalize
+
+def remove_accents(string):
+    return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
 
 file = open('assets/municipios.txt', 'r', encoding='utf8')
 
 for line in file:
-    print(line.lower())
+    print(remove_accents(line.lower()))
 
 file.close()
